9e6b1db4f6ba745e5a0a6ed8713011fdef4c87be
alifalahati <ali.falahati@sdui.de>
2023-06-26 02:05:44 +0200

    fix running scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,30 +4,14 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('queue:work')->everyMinute()->withoutOverlapping()->then(function () {
-            Log::info('Queue worker finished');
-        });
-
-        if (DB::table('failed_jobs')->count() > 0) {
-            $schedule->command('queue:retry all')->everyMinute()->withoutOverlapping()->then(function () {
-                Log::info('Queue retry finished');
-            });
-        }
+        $schedule->exec('php artisan queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
     }
 
-    /**
-     * Register the commands for the application.
-     */
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
